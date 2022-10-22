98becd3507b9b5001ab8093e1e2deade3ec14ace
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-10-22 18:36:45 +0300

    made it so that if build crashed at any moment the script exits

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
   public static void main(String[] args) {
-    HelloWorld helloWorld = new HelloWorld();
-    System.out.println(helloWorld.helloWorld);
+    System.out.println("Hello, World!");
   }
 }
