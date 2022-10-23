5d84b3687332afb0518482a177146a0519eb41cd
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-10-23 23:13:41 +0300

    added to-do

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-echo "Building source..." &&
-javac ../src/*.java -d ./ &&
-echo "Creating jar file..." &&
-jar cmf ./manifest.mf ./output/CoffeeDB.jar ./*.class &&
-echo "Cleaning up..." &&
-rm ./*.class
---
+++ to-do.md
@@ -0,0 +1,3 @@
+# To-do:
+ - Fix command outputs and commands having to have a line feed at the end
+ - Create database drivers
