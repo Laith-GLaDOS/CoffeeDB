94eea824bfad549a6b0aa715695abe6330f74f45
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-10-23 19:42:56 +0300

    remove unused import from Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.net.ServerSocket;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
