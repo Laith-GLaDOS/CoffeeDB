041fdd6d66ed8819b8b03aff15d2f00210aa08ca
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-10 15:01:16 +0300

    added dependency injector to build tool

--- service/build/build.sh
+++ service/build/build.sh
@@ -1,6 +1,9 @@
 echo "Building source..." &&
 javac -cp ../lib/*.jar ../src/*.java -d ./ &&
-echo "Creating jar file..." &&
-jar cmf ./manifest.mf ./output/CoffeeDB.jar ./*.class &&
+echo "Unpacking dependencies..." &&
+unzip ../lib/*.jar > /dev/null &&
+echo "Creating jar file with dependencies injected..." &&
+jar cmf ./manifest.mf ./output/CoffeeDB.jar ./*.class ./ziph &&
 echo "Cleaning up..." &&
-rm ./*.class
+rm ./*.class &&
+rm -r ziph META-INF
