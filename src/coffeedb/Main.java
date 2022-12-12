cb0a58d5a67651df9d5d3503d563637623866d56
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-12 21:02:14 +0300

    added some logs

--- src/coffeedb/Main.java
+++ src/coffeedb/Main.java
@@ -8,20 +8,25 @@ public class Main {
     String configFilePath = args.length >= 1 ? args[0] : "./coffeedb_config.json";
     Config config = null;
 
+    Logger.log(LogType.NORMAL, new StringBuilder("Loading config file (").append(configFilePath).append(")...").toString());
+
     try {
       config = new Config(configFilePath);
     } catch (ConfigFileNotFoundException e) {
-      Logger.log(LogType.ERROR, new StringBuilder("Config file (").append(configFilePath).append(") not found").toString());
+      Logger.log(LogType.ERROR, "Config file not found");
       return;
     } catch (InvalidJSONException e) {
-      Logger.log(LogType.ERROR, new StringBuilder("Config file (").append(configFilePath).append(") has invalid JSON").toString());
+      Logger.log(LogType.ERROR, "Config file has invalid JSON");
       return;
     } catch (InvalidConfigException e) {
-      Logger.log(LogType.ERROR, new StringBuilder("Config file (").append(configFilePath).append(") has invalid configuration").toString());
+      Logger.log(LogType.ERROR, "Config file has invalid configuration");
       return;
     } catch (IOException e) {
-      Logger.log(LogType.ERROR, new StringBuilder("IO error while reading config file (").append(configFilePath).append(") - ").append(e.getMessage()).toString());
+      Logger.log(LogType.ERROR, new StringBuilder("IO error while reading config file - ").append(e.getMessage()).toString());
       return;
     }
+
+    Logger.log(LogType.NORMAL, new StringBuilder("port = ").append(config.data.port).toString());
+    Logger.log(LogType.NORMAL, new StringBuilder("password = ").append(config.data.password).toString());
   }
 }
